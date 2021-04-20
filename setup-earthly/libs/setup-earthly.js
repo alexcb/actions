5db3b982c2a41557d1756109cda37204de6c6d2e
Alex Couture-Beil <alex@earthly.dev>
2021-04-20 15:12:31 -0700

    wip

--- setup-earthly/libs/setup-earthly.js
+++ setup-earthly/libs/setup-earthly.js
@@ -5,7 +5,7 @@ const child_process = require('child_process');
 try {
   console.log(`Setting up earthly`);
 
-  child_process.exec("touch /hello", (error, stdout, stderr) => {
+  child_process.exec("sudo /bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap') => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
