9b6a11560de3eb6a1a87eb68e1938a1db88b9755
Alex Couture-Beil <alex@earthly.dev>
2021-04-20 15:20:21 -0700

    wip

--- setup-earthly/libs/setup-earthly.js
+++ setup-earthly/libs/setup-earthly.js
@@ -2,10 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const child_process = require('child_process');
 
+const IS_WINDOWS = process.platform === "win32";
+const IS_MAC = process.platform === "darwin";
+
 try {
   console.log(`Setting up earthly`);
 
-  child_process.exec("sudo /bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly'", (error, stdout, stderr) => {
+  install_cmd = "sudo /bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly'"
+  if (IS_WINDOWS) {
+    install_cmd = "sudo /bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly'"
+  }
+  else if (IS_MAC) {
+    install_cmd = "brew install earthly"
+  }
+
+  child_process.exec(, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
